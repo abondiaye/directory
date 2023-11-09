f8e99207e87af8faad98fc76e06bb77b66620ea6
abondiaye <a_ndiaye@outlook.com>
2023-11-09 22:17:59 +0100

    add: laravel

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,115 @@
+import { Dropdown } from 'bootstrap';
 
-import '../css/app.scss';
+import { gsap } from 'gsap';
 
-// Importez GSAP pour une utilisation globale
-const { gsap } = window;
+document.addEventListener("DOMContentLoaded", function () {
 
-// Animation de l'élément
-gsap.to("#animated-element", {
-    duration: 2, // Durée de l'animation en secondes
-    x: 100, // Animation de translation sur l'axe X
-    rotation: 360, // Rotation de 360 degrés
-    opacity: 0.7, // Réglage de l'opacité
-    scale: 1.2, // Mise à l'échelle à 120%
-    ease: "power2.out" // Courbe d'animation (easing)
+    // Animation GSAP pour ".element"
+    gsap.to(".element", { duration: 2, x: -50, opacity: 1 });
+
+    // Open close dropdown on click
+    $("li.dropdown").click(function () {
+        if ($(this).hasClass("open")) {
+        $(this).find(".dropdown-menu").slideUp("fast");
+        $(this).removeClass("open");
+        } else {
+        $(this).find(".dropdown-menu").slideDown("fast");
+        $(this).toggleClass("open");
+        }
+    });
+
+    // Close dropdown on mouseleave
+    $("li.dropdown").mouseleave(function () {
+        $(this).find(".dropdown-menu").slideUp("fast");
+        $(this).removeClass("open");
+    });
+
+    // Navbar toggle
+    $(".navbar-toggle").click(function () {
+        $(".navbar-collapse").toggleClass("collapse").slideToggle("fast");
+    });
+
+    gsap.from(title, {
+        duration: 0.8, // Durée plus courte
+        opacity: 0,
+        x: -50, // Déplacement vers la gauche
+        ease: 'power2.out',
+        delay: 0.8
+    });
+    
+
+    // Navbar colors
+    $("#nav-colors > .btn").click(function () {
+        const colorMap = {
+        pink: "#ff5858",
+        red: "#d31027",
+        purple: "#41295a",
+        blue: "#396afc",
+        green: "#add100",
+        yellow: "#f7971e",
+        orange: "#e43a15",
+        };
+        const selectedColor = $(this).attr("id");
+        const backgroundColor = `linear-gradient(to right, ${colorMap[selectedColor]}, ${
+        selectedColor === "pink" ? "#f857a6" : colorMap[selectedColor]
+        })`;
+        $(".navbar").css("background", backgroundColor);
+        $(".dropdown-menu").css("background", colorMap[selectedColor]);
+    });
+
+    // Font colors
+    $("#font-colors > .btn").click(function () {
+        const fontColor = $(this).is("#white") ? "white" : "black";
+        $(".navbar .fa, .link, a").css("color", fontColor);
+        $(".icon-bar").css("background", fontColor);
+    });
+
+    // Edges
+    $("#edges > .btn").click(function () {
+        const edgeType = $(this).is("#rounded") ? "8px" : "0";
+        $(".navbar, .form-control").css("border-radius", edgeType);
+        if ($(window).width() > 768) {
+        $(".dropdown-menu").css({
+            "border-bottom-right-radius": edgeType,
+            "border-bottom-left-radius": edgeType,
+        });
+        }
+    });
+
+    const navIcons = document.querySelectorAll('.nav-icon');
+
+    // Créez une animation GSAP pour chaque icône
+    navIcons.forEach((icon) => {
+        const tl = gsap.timeline({ paused: true });
+
+        tl.to(icon, { x: 10, duration: 0.2 }); // Animation de déplacement horizontal de 10px
+
+        icon.addEventListener('mouseenter', () => {
+        tl.play();
+        });
+
+        icon.addEventListener('mouseleave', () => {
+        tl.reverse();
+        });
+    });
+
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 1,
+        spaceBetween: 20,
+        effect: 'fade',
+        loop: true,
+        speed: 300,
+        mousewheel: {
+        invert: false,
+        },
+        pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true
+        },
+        navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+        }
+    });
 });
